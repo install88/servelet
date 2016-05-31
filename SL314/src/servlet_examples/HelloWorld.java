a04cf1e1150434244d9c4fb897b0f2a79569b0a0
install88 <installandy@gmail.com>
2016-05-31 20:54:49 +0800

    Hello

--- SL314/src/servlet_examples/HelloWorld.java
+++ SL314/src/servlet_examples/HelloWorld.java
@@ -16,6 +16,7 @@ public class HelloWorld extends HttpServlet {
     out.println("<HEAD><TITLE>Hello World</TITLE></HEAD>");
     out.println("<BODY>");
     out.println("<BIG>Hello World , 世界你好 !</BIG>");
+    out.println("<BIG>Hello World , 世界你好 !</BIG>");
     out.println("</BODY></HTML>");
   }
 }
